9444bb73528badd880fcfa9957dd29ba2a34423e
danale <danale@danales-MacBook-Pro.local>
2017-02-08 13:02:20 -0500

    Developed Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
-import { combineReducers } from 'redux';
+// Step 2. In index.js we will wire it in so that it will work
+import {combineReducers} from 'redux';
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    // this gives us the same mapping of state
+    books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,14 @@
+// Step 1. Developing Our Reducer.
+// Step 2. In index.js we will wire it in so that it will work
+// this is a reducer, just a function and a return with a piece of our application state, a list of books
+// to be able to make use of this reducer function anywhere else in our application, we need to export it.
+// with export default any other file in our application can import this one and it will just automatically
+// receive the books reducer.
+export default function() {
+  return [ // return array
+    {title: 'Coding with Javascript for Dummies'}, // these will be a couple of different books
+    {title: 'Meaningful Work'},
+    {title: 'Head First Javascript'},
+    {title: 'How To Do Everything With Javascript'},
+  ]
+}
